384c0eaca5bf22e18e3489d86e7de4ee7744e0d3
SleepWalker <vityagorbenko@yandex.ru>
2016-08-01 00:32:05 +0300

    build successful

---
+++ src/Runner.java
@@ -0,0 +1,43 @@
+import exceptions.ExecutorException;
+import implementations.ExecutorImplementation;
+import implementations.LongTask;
+import implementations.NumberValidator;
+
+import java.util.List;
+
+/**
+ * Created by User on 31.07.2016.
+ */
+public class Runner {
+    public static void main(String[] args) throws ExecutorException {
+
+        ExecutorImplementation executorImplementation = new ExecutorImplementation();
+        NumberValidator validator = new NumberValidator();
+
+        LongTask longTask1 = new LongTask(5L);
+        LongTask longTask2 = new LongTask(10L);
+        LongTask longTask3 = new LongTask(-10L);
+        LongTask longTask4 = new LongTask(-40L);
+
+        longTask1.execute();
+        longTask2.execute();
+        longTask3.execute();
+        longTask4.execute();
+
+        executorImplementation.addTask(longTask1);
+        executorImplementation.addTask(longTask2, validator);
+        executorImplementation.addTask(longTask3);
+        executorImplementation.addTask(longTask4, validator);
+
+        executorImplementation.execute();
+
+        List<Long> actualValidList = executorImplementation.getValidResults();
+        List<Long> actualInvalidList = executorImplementation.getInvalidResults();
+
+        System.out.println("Valid values are:");
+        actualValidList.forEach(System.out::println);
+
+        System.out.println("Invalid values are:");
+        actualInvalidList.forEach(System.out::println);
+    }
+}
